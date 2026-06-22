58b0c77a118f0720697d3fef8d0bf831e73c274b
AnastasiiaVolkoff <reginaprincess24@yandex.ru>
2026-06-22 19:01:40 +0300

    коммит 3

--- lesson5/test_lesson05_task1.py
+++ lesson5/test_lesson05_task1.py
@@ -1,16 +1,21 @@
+from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
 
 
 def test_navigation():
     driver = webdriver.Chrome()
-
+    wait = WebDriverWait(driver, 10)
     # Шаг 1: открыть главную страницу
     driver.get("https://httpbin.org/")
     main_url = driver.current_url
 
     # Шаг 2: найти и кликнуть по ссылке "HTML Form"
-    link = driver.find_element(By.LINK_TEXT, "HTML Form")
+    link = wait.until(
+            EC.element_to_be_clickable(
+                (By.XPATH, '//a[@href="/forms/post"]')
+            ))
     link.click()
 
     # Шаг 3: проверить, что URL изменился на /forms/post
--- lesson5/test_lesson05_task2.py
+++ lesson5/test_lesson05_task2.py
@@ -1,18 +1,28 @@
+from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
 
 
 def test_form_submission():
     driver = webdriver.Chrome()
+    wait = WebDriverWait(driver, 10)
     driver.get("https://httpbin.org/forms/post")
 
     # Шаг 1: найти поле ввода по атрибуту name="custname"
-    name_field = driver.find_element(By.NAME, "custname")
+    name_field = wait.until(
+            EC.element_to_be_clickable(
+                (By.NAME, "custname")
+            ))
     # Шаг 2: ввести имя
+    name_field.clear()
     name_field.send_keys("Анастасия")
 
     # Шаг 3: найти кнопку Submit по тексту (используем XPath)
-    submit_button = driver.find_element(By.XPATH, "//button[text()='Submit']")
+    submit_button = wait.until(
+        EC.element_to_be_clickable(
+            (By.XPATH, '//button')
+        ))
     submit_button.click()
 
     # Шаг 4: проверить, что URL изменился (стал содержать /post)
